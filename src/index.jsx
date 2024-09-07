ca6b8cc7fa9834e272d2b5d8761a865835fd6620
hjensen1 <haydenjensen90@gmail.com>
2024-09-07 08:20:49 -0700

    Automatically redirect from root page to Decrypto

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
+window.location.href = `${window.location.origin}/decrypto/${window.location.hash}`
+
 ReactDOM.render(
   <React.StrictMode>
     <div className="flex-center h-screen w-screen text-gray-500">This is the root page.</div>
