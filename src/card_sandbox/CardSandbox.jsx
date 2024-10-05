31dab142f0e58694249d2086e5b612fff1db6bd2
hjensen1 <haydenjensen90@gmail.com>
2024-10-04 18:48:55 -0700

    Fix container div size

--- src/card_sandbox/CardSandbox.jsx
+++ src/card_sandbox/CardSandbox.jsx
@@ -17,29 +17,31 @@ export default function CardSandbox() {
   const zones = useSelector((state) => state.zones)
 
   return (
-    <CardRenderer CardComponent={PlayingCard} onCardClick={sandboxCardClicked}>
-      {Object.values(zones).length > 0 && (
-        <div className="p-8">
-          <div className="flex space-x-4">
-            <div className="border-4 border-gray-500 rounded-2xl p-2 flex-1">
-              <Zone id="zone1" style={{ height: 350 }} />
-            </div>
+    <div className="w-full h-full">
+      <CardRenderer CardComponent={PlayingCard} onCardClick={sandboxCardClicked}>
+        {Object.values(zones).length > 0 && (
+          <div className="p-8">
+            <div className="flex space-x-4">
+              <div className="border-4 border-gray-500 rounded-2xl p-2 flex-1">
+                <Zone id="zone1" style={{ height: 350 }} />
+              </div>
 
-            <div className="border-4 border-gray-500 rounded-2xl p-2">
-              <Zone id="deck1" style={{ width: 250, height: 350 }} />
+              <div className="border-4 border-gray-500 rounded-2xl p-2">
+                <Zone id="deck1" style={{ width: 250, height: 350 }} />
+              </div>
+            </div>
+            <div className="flex space-x-4 mt-4">
+              <div className="border-4 border-gray-500 rounded-2xl p-2 flex-1">
+                <Zone id="zone2" style={{ height: 300 }} />
+              </div>
+
+              <div className="border-4 border-gray-500 rounded-2xl p-2">
+                <div style={{ width: 250, height: 300 }} />
+              </div>
             </div>
           </div>
-          <div className="flex space-x-4 mt-4">
-            <div className="border-4 border-gray-500 rounded-2xl p-2 flex-1">
-              <Zone id="zone2" style={{ height: 300 }} />
-            </div>
-
-            <div className="border-4 border-gray-500 rounded-2xl p-2">
-              <div style={{ width: 250, height: 300 }} />
-            </div>
-          </div>
-        </div>
-      )}
-    </CardRenderer>
+        )}
+      </CardRenderer>
+    </div>
   )
 }
